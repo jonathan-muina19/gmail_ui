8ab63b28deef08fd762cf61b96a6749236f79111
muina <jordanmuina@gmail.com>
2025-03-18 17:02:18 -0700

    splash screen completed

---
+++ lib/screens/splash_screen.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+import 'dart:async';
+
+import 'package:gmail_ui/screens/home_screen.dart';
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+
+  // Cette fonction permet d'attendre 5 secondes avant d'afficher la HomeScreen
+   void initState(){
+    super.initState();
+    Timer(Duration(seconds: 5), (){
+      Navigator.pushReplacement(context, MaterialPageRoute(builder: (context)=>HomeScreen()
+      ));
+    });
+  }
+
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [
+          Expanded(
+            child: Center(
+              child: Image.asset('assets/images/email.png'),
+            ),
+          ),
+          Text('Google Workspace', style: TextStyle(
+            fontSize: 26,
+            fontWeight: FontWeight.normal,
+            color: Colors.grey
+          ),
+        ),
+          SizedBox(height: 50)
+        ],
+        
+      ),
+      
+    );
+  }
+}
